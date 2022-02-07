43695b2cb4ced8a4dec50e34dfe7f218c2291e0b
brianptrager <brian.trager@rit.edu>
2022-02-07 11:26:33 -0500

    Add project files.

---
+++ Counter261/Counter261/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Counter261.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Counter261
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
---
+++ Counter261/Counter261/ViewModels/CountViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Counter261.ViewModels
+{
+    public class CountViewModel : BindableObject
+    {
+        public CountViewModel()
+        {
+            IncreaseCount = new Command(OnIncreaseCount);
+        }
+        public ICommand IncreaseCount { private set; get; }
+
+        public void OnIncreaseCount()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s).";
+        }
+        private int count = 0;
+
+        private string countDisplay;
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value != countDisplay)
+                {
+                    countDisplay = value;
+                    OnPropertyChanged("CountDisplay");
+                }
+            }
+        }
+    }
+}
