2271d0e36f7688a27370a21a3e74802dfe4aa5a3
brianptrager <brian.trager@rit.edu>
2022-02-09 11:43:47 -0500

    Completed MVVMHelpers demo

--- Counter261/Counter261/ViewModels/CountViewModel.cs
+++ Counter261/Counter261/ViewModels/CountViewModel.cs
@@ -1,18 +1,34 @@
-﻿using System;
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Forms;
 
 namespace Counter261.ViewModels
 {
-    public class CountViewModel : BindableObject
+    public class CountViewModel : ViewModelBase
     {
+        public ObservableRangeCollection<string> People { get; }
+
         public CountViewModel()
         {
             IncreaseCount = new Command(OnIncreaseCount);
+            Title = "Super Duper Counter";
+            HasIzzy = true;
+            People = new ObservableRangeCollection<string>();
+            CallServerCommand = new AsyncCommand(CallServer);
         }
         public ICommand IncreaseCount { private set; get; }
+        public ICommand CallServerCommand { get; set; }
+
+        async Task CallServer()
+        {
+            var people = new List<string>() { "Babe Ruth", "Jackie Robinson", "Hank Aaron" };
+            await Task.Run(() => People.AddRange(people)); //Notification of collection only occurs ONCE!
+        }
 
         public void OnIncreaseCount()
         {
@@ -25,14 +41,7 @@ namespace Counter261.ViewModels
         public string CountDisplay
         {
             get => countDisplay;
-            set
-            {
-                if (value != countDisplay)
-                {
-                    countDisplay = value;
-                    OnPropertyChanged("CountDisplay");
-                }
-            }
+            set => SetProperty(ref countDisplay, value);
         }
     }
 }
---
+++ Counter261/Counter261/ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter261.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+        public ViewModelBase()
+        {
+            //Do stuff like connect to server
+            //Get results from queries
+        }
+
+        private bool hasIzzy;
+        public bool HasIzzy 
+        { 
+            get => hasIzzy; set => SetProperty(ref hasIzzy, value); 
+        }
+    }
+}
